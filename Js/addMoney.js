1962b9710239e48aa767a8e20b6919607a34fc50
bishalmustafi700 <bishalmustafi700@gmail.com>
2024-09-25 02:03:56 +0600

    modal and button color change done

--- Js/addMoney.js
+++ Js/addMoney.js
@@ -6,22 +6,28 @@ document.getElementById('add-money-button')
         
     const addMoney = getInputFieldValueByID('add-money');
     const donateMoney = getTextFieldValueByID('donate-balance');
+    const successModal = document.getElementById('success-modal');
 
     if (addMoney > 0) {
         newBalance = addMoney+donateMoney;
         document.getElementById('donate-balance').innerText=newBalance;
 
         const totalDonateBalance = getTextFieldValueByID('main-balance');
-        
+
         const totalBalance = totalDonateBalance-addMoney;
+        addDonationToHistory(addMoney);
         document.getElementById('main-balance').innerText=totalBalance;
+        successModal.classList.remove('hidden')
        } else{
 
            alert("Please Enter a Valid DONATION Amount. Thanks!");
        }
 
 })
-
+function closeModal(){
+    const modal = document.getElementById('success-modal')
+    modal.classList.add('hidden')
+}
 
 // feni card calculation
 
@@ -30,6 +36,7 @@ document.getElementById('feni-add-money-button').addEventListener('click', funct
 
     const feniAddMoney = getInputFieldValueByID('feni-add-money-input');
     const feniDonatedBalance= getTextFieldValueByID('feni-main-balance');
+    const successModal = document.getElementById('success-modal');
 
     if (feniAddMoney > 0 ){
         feniNewBalance = feniAddMoney + feniDonatedBalance;
@@ -39,13 +46,18 @@ document.getElementById('feni-add-money-button').addEventListener('click', funct
 
         userTotalBalance = totalBalances - feniAddMoney
         document.getElementById('main-balance').innerText=userTotalBalance
-
+        successModal.classList.remove('hidden')
     }else{
 
         alert("Please Enter a Valid DONATION Amount. Thanks!");
     }
 
 })
+function closeModal(){
+    const modal = document.getElementById('success-modal')
+    modal.classList.add('hidden')
+}
+
 
 // Aid for Injured in the Quota Movement
 
@@ -60,15 +72,22 @@ document.getElementById('feni-add-money-button').addEventListener('click', funct
         if (quotaAddMoneyButton > 0) {
             quotaNewBalance = quotaAddMoneyButton + quotaDonateBalance;
             document.getElementById('quota-balance').innerText = quotaNewBalance
+            const successModal = document.getElementById('success-modal');
 
             const inTotalBalance = getTextFieldValueByID('main-balance')
 
             userInTotalBalance= inTotalBalance - quotaAddMoneyButton
             document.getElementById('main-balance').innerText=userInTotalBalance
+            successModal.classList.remove('hidden')
         } else {
 
             alert("Please Enter a Valid DONATION Amount. Thanks!");
         }
     })
+
+    function closeModal(){
+        const modal = document.getElementById('success-modal')
+        modal.classList.add('hidden')
+    }
 
 
--- Js/utlities.js
+++ Js/utlities.js
@@ -15,3 +15,22 @@ function showSectionById (id){
     document.getElementById('history-section').classList.add('hidden');
     document.getElementById(id).classList.remove('hidden');
 }
+
+
+function addDonationToHistory(amount, city) {
+    console.log(amount);
+    const historyList = document.getElementById('historyList');
+    const donationTime = document.getElementById('time');
+    const listItem = document.createElement('div');
+    const timeList = document.createElement('p');
+    listItem.textContent = `${amount} Taka is Donated for famine-2024 at , Bangladesh 
+    Date : ${new Date().toLocaleString()} GMT +0600 (Bangladesh Standard Time)`;
+
+    // timeList.textContent = `Date : ${new Date().toLocaleString()} GMT +0600 (Bangladesh Standard Time)`;
+    // 96500 Taka is Donated for famine-2024 at Feni, Bangladesh
+    // Donated: $${amount} 
+    // on ${new Date().toLocaleString()}
+
+    historyList.appendChild(listItem);
+    donationTime.appendChild(timeList);
+  }
